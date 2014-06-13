64b8f7b3faffca59819d05818eb202f96e293a80
Nathaniel Smith <nathanielrosssmith@gmail.com>
2014-06-13 13:53:36 +1200

    Changed host name.
    Nick smells

--- config.lib.php
+++ config.lib.php
@@ -11,7 +11,7 @@
 */
 class Config{
 
-	public static $hostname = 'localhost';
+	public static $hostname = 'localpoop';
 	public static $username = '';
 	public static $password = '';
 	public static $database = '';
