3f1a670539619a01751843a8c6b1453330a06b27
Nick Sheffield <numbereft@gmail.com>
2014-06-23 18:27:05 +1200

    Upgraded hash class

--- hash.lib.php
+++ hash.lib.php
@@ -1,37 +1,38 @@
 <?php
 
-/*
-	
-	Password hashing class
-
-	usage   : Static
-	version : 1
-	author  : Nick Sheffield
-
-	=====================================
-
-	Example
-
-	$db->set(array(
-		'email'    => 'user@example.com',
-		'password' => Hash::make_password('123'),
-		'salt'     => Hash::salt()
-	))->insert('tb_users');
-
+/**
+*	
+*	Password hashing class
+*
+*	@version 1
+*	@author  Nick Sheffield
+*
 */
 
 class Hash{
+
+	public static function salt(){
+		return substr(hash('sha256', time()), 0, 13);
+	}
 
 	public static function encrypt($password, $salt){
 		return hash('sha256', substr($salt.$password, 0, 300));
 	}
 
-	public static function salt(){
-		return hash('sha256', time());
+	public static function make($password){
+		# Return the full hash
+		return self::salt() . self::encrypt($password, self::salt());
 	}
 
-	public static function make_password($password){
-		return self::encrypt($password, self::salt());
+	public static function verify($password, $hash){
+		# Extract the salt from the hash
+		$extracted_salt = substr($hash, 0, 13);
+
+		# Check if the full hash is the same as what we get from encrypting the
+		# given pw with the extracted salt
+		$matches = $hash == $extracted_salt . self::encrypt($password, $extracted_salt);
+
+		return $matches;
 	}
 
 }
