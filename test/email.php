0688746d1279fdd9725c7e11c40ff2c758fc2c84
Nick Sheffield <numbereft@gmail.com>
2014-06-30 14:28:15 +1200

    Added email test

---
+++ test/email.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once '../email.lib.php';
+
+$email = new Email();
+
+$email->to      = 'numbereft@gmail.com';
+$email->from    = 'email@example.com';
+$email->subject = 'yo';
+$email->message = 'Hello <h1>Hello</h1>';
+$email->html    = false;
+
+$email->send();
+
+if($email->success){
+	echo 'success';
+}else{
+	echo 'fail';
+}
