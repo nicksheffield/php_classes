cdb0325c3d668dc82aa7270a872d88bb0b87e2ff
Nick Sheffield <numbereft@gmail.com>
2014-06-30 14:28:38 +1200

    Fixed html type not working

--- email.lib.php
+++ email.lib.php
@@ -7,16 +7,24 @@ class Email{
 	public $subject = '';
 	public $message = '';
 	public $html = false;
+	public $success = false;
 
 	public function send(){
 
+		$headers = '';
+
 		if($this->html){
-			$headers  .= 'MIME-Version: 1.0' . "\r\n";
+			$headers .= 'MIME-Version: 1.0' . "\r\n";
 			$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+			$this->message = '<html><head><title>'.$this->subject.'</title></head><body>'.$this->message.'</body></html>';
 		}
 
-		$headers = 'From: '.$this->from."\r\n";
+		$headers .= 'From: '.$this->from."\r\n";
 
-		return mail($this->to, $this->subject, $this->message, $headers);
+		$wrapped_message = wordwrap($this->message, 70, "\r\n");
+
+		$this->success = mail($this->to, $this->subject, $wrapped_message, $headers);
+
+		return $this->success;
 	}
 }
