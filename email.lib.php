1549194fc9ab2e348618de14c8713555f0091641
Nick Sheffield <numbereft@gmail.com>
2014-06-30 17:27:02 +1200

    Changed some stuff

--- email.lib.php
+++ email.lib.php
@@ -6,25 +6,35 @@ class Email{
 	public $from = '';
 	public $subject = '';
 	public $message = '';
-	public $html = false;
+	public $html = true;
 	public $success = false;
 
 	public function send(){
-
 		$headers = '';
 
+		# If html is turned on,
 		if($this->html){
+			# set the headers that enable html
 			$headers .= 'MIME-Version: 1.0' . "\r\n";
 			$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-			$this->message = '<html><head><title>'.$this->subject.'</title></head><body>'.$this->message.'</body></html>';
 		}
 
+		# Set the header for the From address
 		$headers .= 'From: '.$this->from."\r\n";
 
+		# Make sure the message has no lines longer than 70 characters
 		$wrapped_message = wordwrap($this->message, 70, "\r\n");
 
-		$this->success = mail($this->to, $this->subject, $wrapped_message, $headers);
+		# Send the email
+		$this->success = mail(
+			$this->to,
+			$this->subject,
+			$wrapped_message,
+			$headers
+		);
 
+		# Return whether it was successfully sent
 		return $this->success;
 	}
+
 }
