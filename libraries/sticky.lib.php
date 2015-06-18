eb8c96642acda1ee5a3c0ac86374b19d19a0eaa7
Nick Sheffield <numbereft@gmail.com>
2015-06-18 15:17:36 +1200

    Added the sticky library

---
+++ libraries/sticky.lib.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once 'input.lib.php';
+
+class Sticky{
+	
+	private static $data = array();
+	
+	public static function get($var, $alt = null){
+		# Check if the var is in the $data, if so return that
+		if(isset(self::$data[$var])){
+			return self::$data[$var];
+			# else, check if it's in Input, and if so return that
+		}else if(Input::get($var)){
+			return Input::get($var);
+			# else, if we provided an alt, use that
+		}else if($alt != null){
+			return $alt;
+			# If not found at all, return false
+		}else{
+			return false;
+		}
+	}
+	
+	public static function set($var, $val){
+		self::$data[$var] = $val;
+	}
+}
