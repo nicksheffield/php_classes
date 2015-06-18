99c051c73d8bcf5937649daaf4475839f431dce4
Nick Sheffield <numbereft@gmail.com>
2015-06-18 15:17:30 +1200

    Added the input library

---
+++ libraries/input.lib.php
@@ -0,0 +1,30 @@
+<?php
+
+class Input{
+	public static $data = array();
+	
+	public static function get($key){
+		if(isset(self::$data[$key])){
+			return self::$data[$key];
+		}else{
+			return false;
+		}
+	}
+	
+	public static function set($key, $val){
+		self::$data[$key] = $val;
+		return $val;
+	}
+	
+	public function has($key){
+		return isset(self::$data[$key]);
+	}
+	
+	public static function posted(){
+		return !!count(self::$data);
+	}
+}
+
+foreach($_POST as $key => $val){
+	Input::set($key, $val);
+}
