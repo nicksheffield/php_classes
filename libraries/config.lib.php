2c01bee86d55aca3d6a755575a8920e1f7ea990b
Nick Sheffield <numbereft@gmail.com>
2015-06-24 14:03:28 +1200

    Added commenting to config

--- libraries/config.lib.php
+++ libraries/config.lib.php
@@ -10,6 +10,8 @@
 
 class Config{
 
+	# Database connection credentials.
+	# Hostname should almost always be set to 'localhost'.
 	public static $database = [
 		'hostname' => 'localhost',
 		'username' => '',
@@ -17,8 +19,15 @@ class Config{
 		'database' => ''
 	];
 	
+	
+	# This variable allows Auth to autoload the current user.
+	# Put the name of the table that your users are stored in
+	# or leave it blank to disable that feature.
 	public static $auth_table = 'users';
 	
+	
+	# This variable is used to separate your Authentication 
+	# and Cart data in the session, per site.
 	public static $sitename = 'default';
 
 }
